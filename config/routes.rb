e7a840c7730b00882a19c69f7d54b7c332322966
Sumit Agarwal <sumit.aga@gmail.com>
2013-05-05 15:32:30 +0100

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,8 @@ SampleApp::Application.routes.draw do
 
   get "static_pages/help"
 
-  get "pages/home"
+  get "static_pages/about"
 
-  get "pages/contact"
-
-  get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
