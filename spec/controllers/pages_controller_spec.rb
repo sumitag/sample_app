b7a8685f2df95ea1067a1e42e9056db3ee221838
Sumit Agarwal <sumit.aga@gmail.com>
2013-05-05 09:00:10 +0100

    Add a StaticPages controller

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      page.should have_selector("title", 
+                       :text => 
+                         "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
@@ -15,5 +23,12 @@ describe PagesController do
       response.should be_success
     end
   end
+  
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+   end
 
 end
